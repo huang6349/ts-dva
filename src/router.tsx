585fec19f727ad495d1e565801c11342cb4ee083
进击的小野兽 <252683858@qq.com>
2016-10-12 16:02:12 +0800

    修改程序路由为“tsx”文件

---
+++ src/router.tsx
@@ -0,0 +1,16 @@
+import React, { PropTypes } from 'react';
+import { Router, Route } from 'dva/router';
+
+// 加载程序页面
+
+import IndexPage from './routes/IndexPage.js';
+
+// 导出路由配置
+
+export default ({ history }) => {
+  return (
+    <Router history={history}>
+      <Route path="/" component={IndexPage} />
+    </Router>
+  );
+};
