1327ee3c4a61e6a8a12a4d263467d92e60651fb4
进击的小野兽 <252683858@qq.com>
2016-10-12 15:36:34 +0800

    修改程序入口为“tsx”文件

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
+import dva from 'dva';
 import './index.html';
 import './index.less';
-import dva from 'dva';
 
-// 1. Initialize
+// 1. 初始化"dva"
 const app = dva();
 
-// 2. Plugins
+// 2. 注册"dva"插件
 //app.use({});
 
-// 3. Model
+// 3. 注册程序模型
 //app.model(require('./models/example'));
 
-// 4. Router
+// 4. 注册程序路由
 app.router(require('./router'));
 
-// 5. Start
+// 5. 启动项目
 app.start('#root');
