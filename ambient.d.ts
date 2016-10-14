66ca7be46236149f67fd1a7ff54b9ef7dfa864be
进击的小野兽 <252683858@qq.com>
2016-10-14 16:09:02 +0800

    新增“*.css,*.less,*.sass,*.scss,*.json”类型文件加载配置

---
+++ ambient.d.ts
@@ -0,0 +1,39 @@
+/**
+ * 加载"css"文件
+ */
+declare module '*.css' {
+  const content: any;
+  export default content;
+}
+
+/**
+ * 加载"less"文件
+ */
+declare module '*.less' {
+  const content: any;
+  export default content;
+}
+
+/**
+ * 加载"sass"文件
+ */
+declare module '*.sass' {
+  const content: any;
+  export default content;
+}
+
+/**
+ * 加载"scss"文件
+ */
+declare module '*.scss' {
+  const content: any;
+  export default content;
+}
+
+/**
+ * 加载"json"文件
+ */
+declare module '*.json' {
+  const content: any;
+  export default content;
+}
