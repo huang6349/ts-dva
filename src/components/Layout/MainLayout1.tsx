0342f7ffe2cf57415ecae2ad17722baa7a4ac90b
进击的小野兽 <252683858@qq.com>
2016-10-16 15:08:58 +0800

    优化

--- src/components/Layout/MainLayout1.tsx
+++ src/components/Layout/MainLayout1.tsx
@@ -8,7 +8,7 @@ import Sidebar from '../Views/Sidebar1';
 
 // 加载布局样式
 
-import styles from './style/MainLayout1.less';
+const styles = require('./style/MainLayout1.less');
 
 const MainLayout = ({ children, location }) => {
   return (
