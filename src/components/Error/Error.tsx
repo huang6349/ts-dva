1674481a3c7bd9f8219407cfe59e3468414f5295
进击的小野兽 <252683858@qq.com>
2016-11-04 11:15:32 +0800

    新增“显示错误信息”页面

---
+++ src/components/Error/Error.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { Icon } from 'antd';
+
+export interface ComponentProps {
+  message: string;
+};
+
+/**
+ * 显示错误信息
+ */
+export default class Component extends React.Component<ComponentProps, any> {
+  static propTypes = {
+    message: React.PropTypes.string.isRequired,
+  };
+
+  render() {
+    return (
+      <section className="text-c pt-20 pb-20">
+        <div style={{ fontSize: '32px' }}><Icon type="frown-o" /></div>
+        <h1 className="mt-15">{this.props.message || '404 Not Found'}</h1>
+      </section>
+    );
+  };
+};
