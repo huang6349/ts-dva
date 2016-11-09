e4690a11d3e759a75ddff3491d6db2b64ecb5748
进击的小野兽 <252683858@qq.com>
2016-11-09 14:32:28 +0800

    提交typings下载的d.ts文件

---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/qs/index.d.ts" />
+/// <reference path="globals/react-dom/index.d.ts" />
+/// <reference path="globals/react/index.d.ts" />
+/// <reference path="globals/underscore/index.d.ts" />
